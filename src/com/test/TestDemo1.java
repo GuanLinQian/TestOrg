b7650c648256989bacc7b1f17e277aaf6d76f29a
lisi <lisi@qq.com>
2019-05-20 20:06:00 +0800

    新建源码

---
+++ src/com/test/TestDemo1.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * 时间:20:05
+ * 作者：Maibenben
+ **/
+public class TestDemo1 {
+    public static void main(String[] args) {
+        System.out.println("ssssssssssssssssssssssssssssss");
+    }
+}
